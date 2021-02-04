94c4536277546e051aaa7ec7bb433490549deec5
raed <52961364+gitRaed@users.noreply.github.com>
2021-02-04 11:56:49 +0100

    environnement variable
    With dotenv.
    PS: Please, do not use ; in .env file..

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
-import express from "express";
+require("dotenv").config();
+const express = require("express");
 
 
 const app = express();
-const port = 3005;
+const port = process.env.PORT || 9000;
 
 app.listen(port, () => {
 
-    console.log('Server is up');
-})
+    console.log('Server is up on ' + process.env.port);
+});
