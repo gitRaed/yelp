099813fa9ab31d27f70374cc209a7f9728c77854
raed <52961364+gitRaed@users.noreply.github.com>
2021-02-04 12:41:34 +0100

    Creating route
    Creating route, now have to check middleware for the post request

--- backend/server.js
+++ backend/server.js
@@ -4,18 +4,33 @@ const express = require("express");
 
 const app = express();
 
-app.get("/getRestaurants", (req, res) => {
+
+// get all restaurants
+app.get("/api/v1/restaurants", (req, res) => {
 
     res.status(200);
     res.json({
-        "status" : "success",
-        "restaurants" : "mcdonald"
+        status : "success",
+        data : {
+            restaurant : ["mcdonald", "wendys", "ribs"]
+        } 
     });
-})
+});
+
+
+//Get a restaurant
+app.get("/api/v1/restaurants/:id", (req, res) => {
+
+    console.log(req.params);
+});
+
+
+//Create a restaurant
+app.post("/api/v1/restaurants", (req, res) => {
+
+    console.log(req.body);
+});
 
 
 const port = process.env.PORT || 9000;
-app.listen(port, () => {
-
-    console.log('Server is up on ' + process.env.port);
-});
+app.listen(port);
