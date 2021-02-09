d3b86652596a288516e1c5bcac6dc09d0a1c3a7d
raed <52961364+gitRaed@users.noreply.github.com>
2021-02-09 12:49:01 +0100

    Fin création route + ajout de middleware

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
 const express = require("express");
-
-
+const morgan = require("morgan");
 const app = express();
 
+app.use(express.json());
 
 // get all restaurants
 app.get("/api/v1/restaurants", (req, res) => {
@@ -29,6 +29,22 @@ app.get("/api/v1/restaurants/:id", (req, res) => {
 app.post("/api/v1/restaurants", (req, res) => {
 
     console.log(req.body);
+});
+
+
+//Update restaurants 
+app.put("/api/v1/restaurants/:id", (req, res) => {
+
+    console.log(req.params.id);
+    console.log(req.body);
+});
+
+//Delete restaurants
+app.delete("/api/v1/restaurants/:id", (req, res) => {
+
+    res.status(204).json({
+        status : "success"
+    });
 });
 
 
