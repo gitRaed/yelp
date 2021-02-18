f42d84671a11a2f466d80e667cac2c1503086db9
raed <52961364+gitRaed@users.noreply.github.com>
2021-02-18 11:07:02 +0100

    sql file added to help people who wanna test my yelp clone !

---
+++ reviewTable.sql
@@ -0,0 +1,7 @@
+CREATE TABLE reviews (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    restaurant_id BIGINT NOT NULL REFERENCES restaurants(id),
+    name VARCHAR(50) NOT NULL,
+    review TEXT NOT NULL,
+    rating INT NOT NULL CHECK(rating >=1 and rating <=5)
+);
