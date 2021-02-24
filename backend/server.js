ed0cdee703d7e82c8bb2bedc8aa214cc7ee78dab
raed <52961364+gitRaed@users.noreply.github.com>
2021-02-24 10:39:40 +0100

    back now returns count and average for rating system

--- backend/server.js
+++ backend/server.js
@@ -13,13 +13,14 @@ app.get("/api/v1/restaurants", async (req, res) => {
 
     try {
         
-        const results = await db.query("SELECT * FROM restaurants");
+        //const results = await db.query("SELECT * FROM restaurants");
+        const restaurantsRatingData = await db.query('select * from restaurants left join(select restaurant_id, count(*), trunc(avg(rating),1) as average_rating from reviews group by restaurant_id) reviews on restaurants.id = reviews.restaurant_id;');
         
         res.status(200).json({
             status: "success",
-            results: results.rows.length,
+            results: restaurantsRatingData.rows.length,
             data: {
-                restaurants: results.rows
+                restaurants: restaurantsRatingData.rows
             }
         });
 
@@ -42,7 +43,7 @@ app.get("/api/v1/restaurants/:id", async (req, res) => {
     try {
         
         //? get data from a restaurant
-        const restaurant = await db.query('Select * from restaurants where id = $1', [req.params.id]);
+        const restaurant = await db.query('select * from restaurants left join(select restaurant_id, count(*), trunc(avg(rating),1) as average_rating from reviews group by restaurant_id) reviews on restaurants.id = reviews.restaurant_id where id = $1', [req.params.id]);
 
         //? get the reviews for that restaurant
         const reviews = await db.query('Select * from reviews where restaurant_id = $1', [req.params.id]);
